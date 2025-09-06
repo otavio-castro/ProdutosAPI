bc765b41f7c0d0bc50fd15a7fc18901ccecbd11a
otavio-castro <ocastro@sga.pucminas.br>
2025-09-06 17:15:28 -0300

    Feat: altera Id para ProdutoId na entidade produto

--- otavio-877229-persistencia-dados/Migrations/20250906201438_AlterarColunaProdutoId.Designer.cs
+++ otavio-877229-persistencia-dados/Migrations/20250906201438_AlterarColunaProdutoId.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using otavio_877229_persistencia_dados.Context;
 
@@ -10,9 +11,11 @@ using otavio_877229_persistencia_dados.Context;
 namespace otavio_877229_persistencia_dados.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250906201438_AlterarColunaProdutoId")]
+    partial class AlterarColunaProdutoId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ otavio-877229-persistencia-dados/Migrations/20250906201438_AlterarColunaProdutoId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace otavio_877229_persistencia_dados.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterarColunaProdutoId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Produtos",
+                newName: "ProdutoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProdutoId",
+                table: "Produtos",
+                newName: "Id");
+        }
+    }
+}
